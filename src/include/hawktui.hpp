6208b7b722a439692bddf16d9ecd8fb2f53e30be
NeoSahadeo <neosahadeo@protonmail.com>
2025-12-06 13:36:16 +0200

    [refac] Ported over all exisiting C code into cpp; Oh boy, I think I'm starting to like it :(

--- src/include/hawktui.hpp
+++ src/include/hawktui.hpp
@@ -1,14 +1,119 @@
 #include <ncurses.h>
-#include <iostream>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
 #include <memory>
+#include <string>
 #include <vector>
 #ifndef HAWKTUI_H
 #define HAWKTUI_H
 
+enum class TypeId { Ctx, Box, Button, Label };
+
+enum class TypeFlags : uint8_t {
+  None = 0,
+  Draggable = 1,
+};
+
+// template<typename Enum, typename = std::enable_if_t<std::is_enum_v<Enum>>>
+// constexpr Enum bitwise_op(Enum x, Enum y, auto op) noexcept {
+//   using underlying = std::underlying_type_t<Enum>;
+//   return static_cast<Enum>(op(static_cast<underlying>(x),
+//   static_cast<underlying>(y)));
+// }
+
+constexpr TypeFlags operator|=(TypeFlags& x, TypeFlags y) noexcept {
+  x = static_cast<TypeFlags>(static_cast<uint8_t>(x) | static_cast<uint8_t>(y));
+  return x;
+}
+
+constexpr bool operator&(TypeFlags x, TypeFlags y) noexcept {
+  return static_cast<bool>(static_cast<uint8_t>(x) & static_cast<uint8_t>(y));
+}
+
+template <typename Derived, typename Base>
+std::unique_ptr<Derived> static_unique_ptr_cast(
+    std::unique_ptr<Base>&& ptr) noexcept {
+  return std::unique_ptr<Derived>(static_cast<Derived*>(ptr.release()));
+}
+
 class UIElement {
  public:
+  std::vector<std::shared_ptr<UIElement>> children;
+  TypeFlags flags = TypeFlags::None;
+  WINDOW* window = nullptr;
+
+  virtual TypeId type() const = 0;
   virtual ~UIElement() = default;
   virtual void render() = 0;
+
+  /**
+   * Add a UIElement to the children of the current element
+   * */
+  void add_child(std::shared_ptr<UIElement> child) {
+    children.emplace_back(child);
+  }
+};
+
+/**
+ * Creates a UIBox element
+ * */
+class UIBox : public UIElement {
+ public:
+  size_t width;
+  size_t height;
+  int x;
+  int y;
+
+  UIBox(size_t w = 10,
+        size_t h = 5,
+        int x = 0,
+        int y = 0,
+        bool draggable = false)
+      : width(w), height(h), x(x), y(y) {
+    window = newwin(height, width, this->y, this->x);
+    flags = TypeFlags::None;
+    if (draggable) {
+      flags |= TypeFlags::Draggable;
+    }
+  }
+
+  ~UIBox() { delwin(window); }
+
+  void render() override {
+    box(window, 0, 0);
+    touchwin(window);
+    wnoutrefresh(window);
+  }
+
+  TypeId type() const override { return TypeId::Box; };
+};
+
+class UITextiBox : public UIBox {
+ public:
+  std::string text;
+  int t_x;
+  int t_y;
+
+  UITextiBox() = default;
+  UITextiBox(std::string text, int x = 0, int y = 0)
+      : UIBox(), text(text), t_x(x), t_y(y) {}
+
+  UITextiBox(size_t b_w,
+             size_t b_h,
+             int b_x,
+             int b_y,
+             std::string text,
+             int x = 0,
+             int y = 0)
+      : UIBox(b_w, b_h, b_x, b_y), text(text), t_x(x), t_y(y) {}
+
+  void render() override {
+    UIBox::render();
+    mvwprintw(window, t_y, t_x, "%s", text.c_str());
+    touchwin(window);
+    wnoutrefresh(window);
+  }
 };
 
 /**
@@ -18,7 +123,6 @@ class UIContext : public UIElement {
  public:
   WINDOW* window;
   mmask_t oldmask;
-  std::vector<std::unique_ptr<UIElement>> children;
 
   UIContext() {
     window = initscr();
@@ -40,18 +144,63 @@ class UIContext : public UIElement {
     mousemask(oldmask, NULL);
     delwin(window);
     endwin();
+
+    children.clear();
   }
 
-  void add_child(std::unique_ptr<UIElement> child) {
-    children.push_back(std::move(child));
+  void start() {
+    render();
+    int c;
+    MEVENT event;
+    struct Coords {
+      int x, y;
+    };
+    Coords click_offset{0};
+    std::shared_ptr<UIElement> current_element;
+    while ((c = wgetch(window)) != 'q') {
+      touchwin(stdscr);
+      wnoutrefresh(window);
+
+      if (c == KEY_MOUSE) {
+        while (getmouse(&event) == OK) {
+          if (current_element && current_element->type() == TypeId::Box) {
+            auto box = std::dynamic_pointer_cast<UIBox>(current_element);
+            mvwin(box->window, event.y - click_offset.y,
+                  event.x - click_offset.x);
+          }
+
+          if (event.bstate & BUTTON1_PRESSED) {
+            for (auto& child : children) {
+              if (child->flags & TypeFlags::Draggable &&
+                  child->type() == TypeId::Box) {
+                auto box = std::dynamic_pointer_cast<UIBox>(child);
+                int start_y, start_x;
+                getbegyx(child->window, start_y, start_x);
+                if (event.x >= start_x && event.x <= start_x + box->width &&
+                    event.y >= start_y && event.y <= start_y + box->height) {
+                  current_element = child;
+                  click_offset.x = event.x - start_x;
+                  click_offset.y = event.y - start_y;
+                }
+              }
+            }
+          } else if (event.bstate & BUTTON1_RELEASED) {
+            current_element.reset();
+          }
+        }
+      }
+      render();
+    }
   }
 
-  void render() {
+  void render() override {
+    wnoutrefresh(window);
     for (auto& child : children)
       child->render();
+    doupdate();
   }
-};
 
-class Box {};
+  TypeId type() const override { return TypeId::Ctx; };
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <unistd.h>
+#include <memory>
 #include "include/hawktui.hpp"
 
 int main() {
   UIContext* ctx = new UIContext();
-  sleep(1);
+  // auto box = std::make_shared<UIBox>(10, 5, 0, 0, true);
+  // ctx->add_child(box);
+  auto textibox =
+      std::make_shared<UITextiBox>(15, 5, 0, 0, "Hello, world!", 1, 2);
+  textibox->flags |= TypeFlags::Draggable;
+  ctx->add_child(textibox);
+  ctx->start();
   delete ctx;
   return 0;
 }
